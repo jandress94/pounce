22d2b81c3655f0c7edc85741975b9c4aeea59203
Jim Andress <jandress@lirio.co>
2020-06-12 23:39:04 -0700

    Change numbers back to real values

--- pounce/src/client/js/app/game/game.model.js
+++ pounce/src/client/js/app/game/game.model.js
@@ -1,7 +1,7 @@
 game.model = (function () {
 
-    let NUM_POUNCE_CARDS = 1;
-    let POINTS_TO_WIN = 3;
+    let NUM_POUNCE_CARDS = 13;
+    let POINTS_TO_WIN = 100;
 
     let pounce_pile;
     let build_piles;
