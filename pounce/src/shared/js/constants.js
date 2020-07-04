fddfe2b24ef7d2155f723df10d747b9dd1d796d1
Jim Andress <jandress@lirio.co>
2020-07-04 13:48:24 -0700

    switch constants to true game values

--- pounce/src/shared/js/constants.js
+++ pounce/src/shared/js/constants.js
@@ -1,7 +1,7 @@
 (function(exports){
-    exports.POINTS_TO_WIN = 1;
-    exports.NUM_POUNCE_CARDS = 1;
-    exports.NUM_DITCH_BEFORE_END_HAND = 2;
+    exports.POINTS_TO_WIN = 100;
+    exports.NUM_POUNCE_CARDS = 13;
+    exports.NUM_DITCH_BEFORE_END_HAND = 10;
 
 }(typeof exports === 'undefined' ? this.constants = {} : exports));
 
